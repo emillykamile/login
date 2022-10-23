8dd5580e048df0d77550f6fe71f919ea623b725a
emillykamile <emillykamile.contato@gmail.com>
2022-10-23 17:44:05 -0300

    Mudança na validação de Login

--- validacaoLogin.js
+++ validacaoLogin.js
@@ -1,27 +1,45 @@
-var form = document.querySelector("#form-action");
-var email = document.querySelector("#email");
-var senha = document.querySelector("#senha");
-var btn = document.getElementById("#botao-continuar");
+let email = document.getElementById('email');
+let password = document.getElementById('password');
+let form = document.querySelector('form');
+let textForm = document.getElementById('textForm');
+let textEmail = document.getElementById('textEmail');
+
+form.addEventListener('submit', (e) => {
+    if(email.value == '' && password.value == '') {
+        textForm.textContent = "Você precisa preencher todos os campos!"
+    } else {
+        console.log(email.value);
+        console.log(password.value);
+    }
+    e.preventDefault()
+})
+
+email.addEventListener("keyup", () => {
+    if(validatorEmail(email.value) !== true) {
+        textEmail.textContent = "O formato do email deve ser: email.usuario@compasso.com.br"
+    } else {
+        textEmail.textContent = ''
+    }
+})
 
 
-form.addEventListener("submit", (l)=>{
-    
-    var usuario = email.value;
-    var senha = senha.value;
+/* Validação ainda sem funcionar */
+/* btn.addEventListener("submit", () => { 
+    if(validatorEmail() == true && validatorPassword() == true)  {
+        window.location.href = "./page-interna.html";
+    }
+}) */
 
-    if(usuario.value === ' ' && senha === ' '){
-        usuario.style.borderColor = "#E9B425";
-        senha.style.borderColor = "#E9B425";
 
-    }else{
-        if(usuario.value === 'admin@admin.com.br' && senha ==='admin'){
-            window.location.href = "./page-interna.html";
-        }
-        else{
-            btn-continuar.preventDefault();
-            console.log("Senha ou usuário inválidos!")
-            var alert = document.querySelector(".alert");
-            alert.innerText = "Senha ou usuário inválidos!"
-            alert.classList.add("alert");
-        }
-    })
+function validatorEmail(email) {
+    let emailPattern =
+      /^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})$/;
+    return emailPattern.test(email);
+  }
+
+function validatorPassword(password) {
+    let passwordPattern =
+      /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{6,16}$/;
+    return passwordPattern.test(password);
+  }
+
